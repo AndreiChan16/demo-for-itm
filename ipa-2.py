c03c6a9f0fd0de4f52394ae6aaf858eb3a43e2de
AndreiChan16 <andrei.chan@obf.ateneo.edu>
2023-07-07 02:15:10 +0800

    Second commit

--- ipa-2.py
+++ ipa-2.py
@@ -11,7 +11,6 @@ def shift_letter(letter, shift):
             return(new_letter)
     else:
         new_letter=alphabet_list[(alphabet_list.index(letter)+shift)%26]
-        print(new_letter)
         return(new_letter)
     
 letters=str(input("Add a letter:"))
@@ -22,19 +21,22 @@ shift_letter(letters.upper(), shifts)
     
 def caesar_cipher(message, shift):
     alphabet_list=["A","B", "C", "D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    new_message=""
     for i in range (0, len(message)):
         if message[i]==" ":
             new_word=" "
-            print(new_word,end="")
+            new_message+=new_word
        
         else:
             new_word= alphabet_list[((alphabet_list.index(message[i])+shift)%26)]
-            print(new_word,end="")
+            new_message+=new_word
+    return new_message
     
 letters=str(input("Add a word:"))
 shifts=int(input("How many shifts to the right?:"))
 
 caesar_cipher(letters.upper(), shifts)
+
 
 def shift_by_letter(letter, shift):
     alphabet_list=["A","B", "C", "D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
@@ -43,7 +45,6 @@ def shift_by_letter(letter, shift):
             return(new_letter)
     else:
         new_letter=alphabet_list[(alphabet_list.index(letter)+alphabet_list.index(shift))%26]
-        print(new_letter)
         return(new_letter)
     
 letters=str(input("Add a letter:"))
@@ -53,22 +54,25 @@ shift_by_letter(letters.upper(), shifts.upper())
 
 def vigenere_cipher(message, key):
     alphabet_list=["A","B", "C", "D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    new_message2=""
     for i in range (0, len(message)):
         if message[i]==" ":
             new_word=" "
-            print(new_word,end="") 
+            new_message2+=new_word
+           
            
         else:
             new_word= alphabet_list[((alphabet_list.index(message[i])+(alphabet_list.index(key[i%len(key)])))%26)]
-            print(new_word,end="")
-          
-    
+            new_message2+=new_word
+    return new_message2
+             
 letters=str(input("Add a word:"))
 shifts=str(input("How many shifts to the right?(key word):"))
 
 vigenere_cipher(letters.upper(), shifts.upper())
 
 def scytale_cipher(message, shift):
+    new_message3=""
     if len(message) % shift== 0:
         message= message 
     else:
@@ -76,7 +80,9 @@ def scytale_cipher(message, shift):
             message = message+ "_" 
     for i in range(0,len(message)):
         word= message[(i // shift) +(len(message) // shift) * (i % shift)]
-        print(word,end="")
+        new_message3+=word
+    return new_message3
+    
         
 letters=str(input("Add a word:"))
 shifts=int(input("How many shifts to the right?:"))
@@ -84,9 +90,12 @@ shifts=int(input("How many shifts to the right?:"))
 scytale_cipher(letters.upper(), shifts)
 
 def scytale_decipher(message, shift):
+    new_message4=""
     for i in range (0, len(message)):
         decipher_word= message[(i // int(len(message)/shift)) +(len(message) // int(len(message)/shift)) * (i % int(len(message)/shift))]
-        print(decipher_word, end="")
+        new_message4 += decipher_word
+    return new_message4
+    
 
 letters=str(input("Add a word:"))
 shifts=int(input("How many shifts to the right?:"))
