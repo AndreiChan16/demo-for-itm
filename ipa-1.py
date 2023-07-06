474664f6dae55534c8faff7ce98534a531797957
AndreiChan16 <andrei.chan@obf.ateneo.edu>
2023-07-06 17:29:24 +0800

    Second commit

--- ipa-1.py
+++ ipa-1.py
@@ -19,7 +19,7 @@ def savings (gross_pay, tax_rate, expenses):
     
     if tax_rate<=1 and tax_rate>=0:
         final_pay= int((gross_pay)*(1-tax_rate))-(expenses)
-        return(f"your final pay is: {final_pay} Philippine centavos")
+        return(final_pay)
     else: 
         return("not valid tax value")
     
@@ -39,7 +39,7 @@ savings (gross,tax,expense)
 def material_waste(total_material, material_units, num_jobs, job_consumption):
     waste= total_material- (num_jobs*job_consumption)
     Waste=str(waste)
-    return ("The total remaining material is: " + Waste+ material_units)
+    return (f"{Waste}{material_units}")
 
 totalmaterial= int(input("What is the total material available?:"))
 numjobs= int(input("What are the number of jobs?:"))
@@ -57,7 +57,7 @@ def interest(principal, rate, periods):
     if rate >= 0 and rate<=1: 
         Interest= principal*(rate)*periods
         final= int(principal + Interest)
-        return (f"your final investment is: {final}")
+        return (final)
     else:
         return("invalid interest rate")
 principalI= int(input("Principal investment amount:"))
@@ -71,10 +71,16 @@ interest(principalI, rateI, periodsI)
     
 def body_mass_index(weight, height):
     BMI= (weight*0.4536)/((((height[0]*12)+(height[1]))*0.0254)**2)
-    return(f"your BMI is: {BMI}")
+    return(float(BMI))
 weightI= float(input("Enter your weight in lbs:"))
 heightI= [int(input("How tall are you in feet (disregard excess inches not in a whole foot):")), int(input("how many inches did you exceed of a whole number of a foot (inches component)?:"))]
 
 body_mass_index(weightI, heightI)
     
+
+
+# In[ ]:
+
+
+
 
